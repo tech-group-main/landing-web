04ec731e803bd2f9cea28d35d602b6de1cd3136b
Лучиц Тимофей <maze_98@mail.ru>
2025-03-13 01:25:31 +0500

    Developed header
    Need to change font family

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+export const Main = () => {
+    return (
+        <div className='w-full'>
+            <div className='max-w-[1440px] m-auto border'>
+                <div className='circle circle1'></div>
+                <div className='circle circle2'></div>
+                <div className='circle circle3'></div>
+            </div>
+        </div>
+    );
+};
