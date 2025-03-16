3082ccf919f20b726a9ca24dad2b86674202b7f2
Лучиц Тимофей <maze_98@mail.ru>
2025-03-17 04:29:00 +0500

    fix docker-compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
         react(),
         tailwindcss(),
         svgLoader()],
+    server: {
+        host: true,
+        port: 3000,
+        watch: {
+            usePolling: true,
+        },
+    }
 })
